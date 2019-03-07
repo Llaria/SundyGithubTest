bb29df00a49cbce677c2f6efb132ec18cb607d7d
Sundy <sundistar@163.com>
2019-03-07 22:11:50 +0800

    Coding

--- app/src/main/java/sun/sundy/sundygithubtest/App.java
+++ app/src/main/java/sun/sundy/sundygithubtest/App.java
@@ -1,9 +1,12 @@
 package sun.sundy.sundygithubtest;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
 
 import com.alipay.iot.sdk.APIManager;
 import com.alipay.iot.sdk.InitFinishCallback;
+import com.qihoo360.replugin.RePlugin;
 
 import sun.sundy.sundygithubtest.utils.SpeechSoundManager;
 import sun.sundy.sundygithubtest.utils.ToastUtils;
@@ -18,9 +21,25 @@ public class App extends Application {
     public static App app;
 
     @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        RePlugin.App.attachBaseContext(this);
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
         initApp(this);
+
+        RePlugin.App.onCreate();
+
+//        try {
+//            SDKManager.initSDK(this);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        //初始化皮肤管理器
+//        SkinManager.getInstance().init(this);
 
         if (!SpeechSoundManager.getInstance().initSpeechService())
             ToastUtils.showLazzToast("请确认是否安装讯飞语音+");
@@ -39,6 +58,28 @@ public class App extends Application {
         } catch (APIManager.APIInitException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+        /* Not need to be called if your application's minSdkVersion > = 14 */
+        RePlugin.App.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        /* Not need to be called if your application's minSdkVersion > = 14 */
+        RePlugin.App.onTrimMemory(level);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration config) {
+        super.onConfigurationChanged(config);
+        /* Not need to be called if your application's minSdkVersion > = 14 */
+        RePlugin.App.onConfigurationChanged(config);
     }
 
     private void initApp(App application) {
---
+++ plugintest/src/main/java/sun/sundy/plugintest/PluginOneActivity.kt
@@ -0,0 +1,12 @@
+package sun.sundy.plugintest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PluginOneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plugin_one)
+    }
+}
