2c84e4c90633b85a34123b10830e13383ce2c559
Sundy <dsun17@best-inc.com>
2019-10-29 15:47:52 +0800

    zbar和zxing测试

--- app/src/main/java/sun/sundy/sundygithubtest/SundyApplication.java
+++ app/src/main/java/sun/sundy/sundygithubtest/SundyApplication.java
@@ -6,20 +6,8 @@ import android.util.Log;
 import com.alipay.iot.sdk.APIManager;
 import com.alipay.iot.sdk.InitFinishCallback;
 
-import org.greenrobot.greendao.database.Database;
-
-import java.util.List;
-
-import sun.sundy.sundygithubtest.sql.entity.UserEntity;
-import sun.sundy.sundygithubtest.sql.gen.DaoMaster;
-import sun.sundy.sundygithubtest.sql.gen.DaoSession;
-import sun.sundy.sundygithubtest.sql.gen.UserEntityDao;
-import sun.sundy.sundygithubtest.sql.utils.BizDaoHelper;
-import sun.sundy.sundygithubtest.sql.utils.BizDaoManager;
-import sun.sundy.sundygithubtest.utils.FileUtil;
 import sun.sundy.sundygithubtest.utils.SpeechSoundManager;
 import sun.sundy.sundygithubtest.utils.ToastUtils;
-import sun.sundy.sundygithubtest.utils.WeightPreference;
 
 
 /**
@@ -53,27 +41,27 @@ public class SundyApplication extends Application {
             e.printStackTrace();
             Log.d("dd", "onCreate: ");
         }
-        if (WeightPreference.getInstance(app).isFirstIn()){
-            String dbPath = FileUtil.SDPATH + BizDaoHelper.DB_NAME;
-            Database database = new BizDaoHelper(app,dbPath).getWritableDb();
-            DaoMaster daoMaster = new DaoMaster(database);
-            DaoSession daoSession = daoMaster.newSession();
-            UserEntityDao userEntityDao = daoSession.getUserEntityDao();
-            List<UserEntity> datas = userEntityDao.loadAll();
-            daoSession.clear();
-            database.close();
-            FileUtil.deleteFile(dbPath);
-            BizDaoManager.getInstance().init();
-            for (int i = 0; i < datas.size(); i++) {
-                UserEntity userEntity = new UserEntity();
-                userEntity.setName(datas.get(i).getName());
-                userEntity.setAge(datas.get(i).getAge());
-                UserEntityDao userEntityDao1 = BizDaoManager.getInstance().getDaoSession().getUserEntityDao();
-                userEntityDao1.insert(userEntity);
-            }
-        }else {
-            BizDaoManager.getInstance().init();
-        }
+//        if (WeightPreference.getInstance(app).isFirstIn()){
+//            String dbPath = FileUtil.SDPATH + BizDaoHelper.DB_NAME;
+//            Database database = new BizDaoHelper(app,dbPath).getWritableDb();
+//            DaoMaster daoMaster = new DaoMaster(database);
+//            DaoSession daoSession = daoMaster.newSession();
+//            UserEntityDao userEntityDao = daoSession.getUserEntityDao();
+//            List<UserEntity> datas = userEntityDao.loadAll();
+//            daoSession.clear();
+//            database.close();
+//            FileUtil.deleteFile(dbPath);
+//            BizDaoManager.getInstance().init();
+//            for (int i = 0; i < datas.size(); i++) {
+//                UserEntity userEntity = new UserEntity();
+//                userEntity.setName(datas.get(i).getName());
+//                userEntity.setAge(datas.get(i).getAge());
+//                UserEntityDao userEntityDao1 = BizDaoManager.getInstance().getDaoSession().getUserEntityDao();
+//                userEntityDao1.insert(userEntity);
+//            }
+//        }else {
+//            BizDaoManager.getInstance().init();
+//        }
     }
 
     private void initApp(SundyApplication application) {
